581ce4e68b9e4ab9a933a81b91aafb913936a23a
pls455 <karamhahhss123@gmail.com>
2026-09-16 22:12:39 +0300

    feat: add Flutter application entry point

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: NaghmaApp()));
+}
