53e4e2bf76fc8db317ac7e62cb467aa147f1824e
pls455 <karamhahhss123@gmail.com>
2026-09-16 22:20:54 +0300

    feat: add content provider registry

---
+++ lib/data/services/content_provider_registry.dart
@@ -0,0 +1,9 @@
+import '../../domain/repositories/content_provider.dart';
+import 'direct_url_provider.dart';
+
+class ContentProviderRegistry {
+  ContentProviderRegistry({List<ContentProvider>? providers})
+      : providers = providers ?? const [DirectUrlProvider()];
+
+  final List<ContentProvider> providers;
+}
