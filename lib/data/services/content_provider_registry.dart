ace3a4bdf687c6d9d3854ba57ad5d4f45656108d
pls455 <karamhahhss123@gmail.com>
2026-09-17 12:21:36 +0300

    feat: register YouTube resolver

--- lib/data/services/content_provider_registry.dart
+++ lib/data/services/content_provider_registry.dart
@@ -1,9 +1,13 @@
 import '../../domain/repositories/content_provider.dart';
 import 'direct_url_provider.dart';
+import 'youtube_provider.dart';
 
 class ContentProviderRegistry {
   ContentProviderRegistry({List<ContentProvider>? providers})
-      : providers = providers ?? const [DirectUrlProvider()];
+      : providers = providers ?? const [
+          YoutubeProvider(),
+          DirectUrlProvider(),
+        ];
 
   final List<ContentProvider> providers;
 }
