a7f0fd30d0bcdc9edabf82b6d7713d714360ba40
pls455 <karamhahhss123@gmail.com>
2026-09-17 16:59:17 +0300

    feat: initialize background download service

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
+import 'data/services/download_background_service.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await DownloadBackgroundService.initialize();
   runApp(const ProviderScope(child: NaghmaApp()));
 }
